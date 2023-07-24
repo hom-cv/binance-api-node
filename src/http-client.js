4d25b89cfbc5815febe9fad51f99551212cc6b0d
hom-cv <chaithatvanasrisawasd@gmail.com>
2023-07-24 18:39:10 +0700

    return proxy as well

--- src/http-client.js
+++ src/http-client.js
@@ -207,7 +207,7 @@ const privateCall = ({
       .then(data => {
         const ipAddress = data.ip;
         console.log('My IP Address:', ipAddress);
-        return ipAddress
+        return [ipAddress, proxy];
       })
       .catch(error => {
         console.error('Error fetching IP address:', error);
